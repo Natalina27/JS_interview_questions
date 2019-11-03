3214c439b2464994a9932998a8c7ebe76cc92e73
Natalya <natalyamyunster@yahoo.com>
2019-11-03 15:28:04 +0100

    7 difference data types in js

---
+++ 01_types.js
@@ -0,0 +1,12 @@
+//7 types of data types:
+//null, undefined, boolean, number, string, object, symbol
+console.log( typeof 0);
+console.log( typeof true);
+console.log( typeof 'js'); // "", '', ``
+console.log( typeof undefined);
+console.log( typeof {});
+console.log( typeof Symbol('js') );
+console.log( typeof null);
+console.log( typeof function(){});
+console.log( typeof NaN);
+
