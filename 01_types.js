2d2a99cefb95d8665fa37517af61b39849083fdb
Natalya <natalyamyunster@yahoo.com>
2019-11-03 15:50:06 +0100

    type conversion and Boolean()

--- 01_types.js
+++ 01_types.js
@@ -10,3 +10,20 @@ console.log( typeof null);
 console.log( typeof function(){});
 console.log( typeof NaN);
 
+//Type conversion
+let language = 'JS';
+if(language){ // language === true
+    console.log('the best language is : ', language);
+}
+//false : '', 0, null, undefined, NaN, false
+console.log(Boolean(''));
+console.log(Boolean(0));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));
+console.log(Boolean(false));
+//true : {}, []
+console.log(Boolean({}));
+console.log(Boolean([]));
+
+//Strings and Math
