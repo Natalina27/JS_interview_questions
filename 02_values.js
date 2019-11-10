cd908a70349ea36f7f0640a37c819751d8e30516
Natalya Myunster <natalyamyunster@yahoo.com>
2019-11-10 13:05:30 +0100

    mutable array

--- 02_values.js
+++ 02_values.js
@@ -3,3 +3,12 @@ let b = a;
 b++;
 console.log('a', a);
 console.log('b', b);
+
+//arrays
+
+let arrayA = [1, 2, 3];
+let arrayB = arrayA;
+arrayB.push(4);
+console.log('arrayA', arrayA);
+console.log('arrayB', arrayB);
+
