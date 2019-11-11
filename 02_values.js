46c166222948cc1a508a1f92e8881dc2d6dea1b9
Natalya Myunster <natalyamyunster@yahoo.com>
2019-11-11 15:38:04 +0100

    conca() for createc a copy

--- 02_values.js
+++ 02_values.js
@@ -12,3 +12,10 @@ arrayB.push(4);
 console.log('arrayA', arrayA);
 console.log('arrayB', arrayB);
 
+//concat() create copy of array
+let arrayA1 = [1, 2, 3];
+let arrayB2 = arrayA1.concat();
+arrayB2.push(4);
+console.log('arrayA1', arrayA1);
+console.log('arrayB2', arrayB2);
+
