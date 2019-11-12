773033af0c823f3bf4a6577636c3d180bca32299
Natalya Myunster <natalyamyunster@yahoo.com>
2019-11-12 14:38:20 +0100

    const primitive and references examples

--- 05_let, const.js
+++ 05_let, const.js
@@ -13,3 +13,30 @@ let b = 'variable b';
 console.log(a);
 console.log(b);
 
+//const
+
+const PORT = 8080;
+//PORT = 2000; TypeError: Assignment to constant variable.
+const array = ['JS', 'is', 'aweresome'];
+array.push('!');
+console.log(array);
+array[0] = 'Javascript';
+console.log(array);
+//array = ''; TypeError: Assignment to constant variable.
+//console.log(array);
+
+const obj = {};
+obj.name = 'Natalya';
+obj.age = 44;
+obj.surname = 'Myunster';
+obj.middleName = 'NM';
+
+console.log(obj);
+
+obj.age = 88;
+delete obj.middleName;
+
+console.log(obj);
+// obj = ''; TypeError: Assignment to constant variable.
+//obj = 23; TypeError: Assignment to constant variable.
+//Мы можем менять только внутреннее состояние обьекта
