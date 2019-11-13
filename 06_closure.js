3aa49676f578ee79a89c32e215230832fcbe3e2a
Natalya Myunster <natalyamyunster@yahoo.com>
2019-11-13 09:27:54 +0100

    added practical example of closure

--- 06_closure.js
+++ 06_closure.js
@@ -1,4 +1,4 @@
-//Closure (замыкание ) это когда функция замыкает в себе значения из вышестоящего скоупа
+//Closure (замыкание ) это когда функция замыкает в себе значения из вышестоящего скоупа, но которые не доступны извне
 function sayHello (name){
     const message = 'Hello' + name ;
 
@@ -12,3 +12,23 @@ const helloToIgor = sayHello('Igor');
 console.log(helloToElena);
 helloToElena();
 helloToIgor();
+
+// Practical example
+function createFrameworkManager(){
+    const fw = ['Angular', 'React'];
+    return{
+        print: function(){
+            console.log(fw);
+        },
+        add: function(framework){
+            fw.push(framework);
+        }
+    }
+}
+
+const manager = createFrameworkManager();
+console.log(manager);
+//console.log(fw); ReferenceError: fw is not defined
+manager.print();
+manager.add('VueJS');
+manager.print();
