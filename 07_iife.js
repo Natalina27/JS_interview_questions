6b12cbdae1f1d4d71b9c25bda23610d9e96b9b18
Natalya Myunster <natalyamyunster@yahoo.com>
2019-11-20 13:35:29 +0100

    added new js file about immediately invoking function expression

--- 07_iife.js
+++ 07_iife.js
@@ -2,9 +2,21 @@
 //Моментально вызывающиеся функции
 
 let result = [];
+//1
 for(let i = 0; i < 5; i++){
     result.push(function () {
         console.log(i);
     })
 }
 result[2]();
+result[4]();
+//2 iife
+for(var i = 0; i < 5; i++){
+    (function(){
+        var j = i;
+        result.push(function (){ console.log(j) } )
+    })();
+}
+result[2]();
+result[4]();
+
