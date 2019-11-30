9760bce9000ed51988f8624edfbef17ce5aa3beb
Natalya Myunster <natalyamyunster@yahoo.com>
2019-11-30 09:10:48 +0100

    added function myNew

--- 09_new.js
+++ 09_new.js
@@ -1,4 +1,19 @@
+//new
+
 function Cat (color, name){
     this.color = color;
     this.name = name;
 }
+
+const cat = new Cat('black', 'KOT');
+console.log(cat);
+
+function myNew(constructor, ...args) {
+    const obj ={};
+    Object.setPrototypeOf(obj, constructor.prototype);
+    return constructor.apply(obj, args) || obj
+}
+
+const newCat = myNew(Cat,'black', 'KOT');
+console.log(newCat);
+
