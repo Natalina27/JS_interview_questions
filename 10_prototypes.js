f9bc10aae9535068ef579abc408174e479ae4cc8
Natalya Myunster <natalyamyunster@yahoo.com>
2019-12-12 11:27:14 +0100

    proto

--- 10_prototypes.js
+++ 10_prototypes.js
@@ -27,5 +27,23 @@ console.log(person.legs);
 console.log(person.name);
 
 console.log(person.hasOwnProperty('name'));
+console.log(person.hasOwnProperty('skin'));
+
+//Object.create();
+let proto = { year: 2019};
+const myYear = Object.create(proto);
+
+console.log(myYear.year);
+proto.year = 2200;
+console.log(myYear.year);
+proto = {year: 999};
+console.log(myYear.year);
+
+//console.log(myYear.hasOwnProperty('year') );
+//console.log(myYear.__proto__ === proto );
+
+
+
+
 
 
