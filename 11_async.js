1c4ea508fc35d67dc0798b899adbd57f4e605f13
Natalya Myunster <natalyamyunster@yahoo.com>
2019-12-14 09:17:11 +0100

    asynchronous in js

--- 11_async.js
+++ 11_async.js
@@ -1,1 +1,9 @@
 //asynchronous in js
+const first = () => console.log('First');
+const second = () => console.log('Second');
+const third = () => console.log('Third');
+
+first();
+setTimeout(second, 0);//call stack -event queue
+third();
+
